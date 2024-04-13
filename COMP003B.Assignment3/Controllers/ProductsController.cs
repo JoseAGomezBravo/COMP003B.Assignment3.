6aaf3954fd187fbfa6b882356213030286bdc4ed
JoseAGomezBravo <04josegomez2022@gmail.com>
2024-04-12 21:13:25 -0700

    commit controllers

--- COMP003B.Assignment3/Controllers/ProductsController.cs
+++ COMP003B.Assignment3/Controllers/ProductsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace COMP003B.Assignment3_.Controllers
 {
-    public class ProductController : Controller
+    public class ProductsController : Controller
     {
         public IActionResult Index()
         {
---
+++ COMP003B.Assignment3/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3_.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public decimal Price { get; set; }
+    }
+}
