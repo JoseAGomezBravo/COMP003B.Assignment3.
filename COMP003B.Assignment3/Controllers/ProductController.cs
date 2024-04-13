81d4b66dda9b29ed5f7784e6e7308e8e40e8b297
JoseAGomezBravo <04josegomez2022@gmail.com>
2024-04-12 20:32:57 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3_.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
